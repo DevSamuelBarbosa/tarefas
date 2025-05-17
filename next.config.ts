20abe78d00e3b54d8ff53e810647802317a61176
DevSamuelBarbosa <samuelbarbosa170319@gmail.com>
2025-05-17 09:58:26 -0300

    teste

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+const ambiente = process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'production'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    devIndicators: false,
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    basePath: ambiente ? "/templateadv" : '',
+    assetPrefix: ambiente ? "/templateadv" : '',
+    trailingSlash: true,
 };
 
 export default nextConfig;
