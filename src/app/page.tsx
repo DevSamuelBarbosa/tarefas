b47473352097f4d6f48056b03c2072f486effee3
DevSamuelBarbosa <samuelbarbosa170319@gmail.com>
2025-05-24 10:50:59 -0300

    Feat: Tela de cadastro de usuário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium cum quia distinctio libero, eos earum eveniet aperiam fugit, quis blanditiis non beatae voluptatum minus dolores veniam! Saepe veritatis sapiente quod?</h1>
 		</div>
 	);
 }
---
+++ src/app/usuario/cadastro/page.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { useState } from 'react'
+import api from '@/utils/api'
+import { useRouter } from 'next/navigation'
+import { Eye, EyeOff } from 'lucide-react'
+import { toast } from 'react-hot-toast'
+
+
+export default function CadastroUsuario() {
+	const router = useRouter()
+
+	const [nome, setNome] = useState('')
+	const [email, setEmail] = useState('')
+	const [senha, setSenha] = useState('')
+	const [mostrarSenha, setMostrarSenha] = useState(false)
+	const [erro, setErro] = useState<string | null>(null)
+	const [emailErro, setEmailErro] = useState('')
+	const [carregando, setCarregando] = useState(false)
+
+
+	const handleCadastro = async (e: React.FormEvent) => {
+		e.preventDefault()
+		setErro(null)
+		setCarregando(true)
+
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+		if (!emailRegex.test(email)) {
+			setEmailErro('Email inválido')
+			setCarregando(false)
+			return
+		} else {
+			setEmailErro('')
+		}
+
+		try {
+			await api.post('/auth/registro', { nome, email, senha })
+			toast.success(`Usuário ${nome} foi cadastrado com sucesso!`, {duration: 5000})
+			router.push('/usuario/login')
+		} catch (err: any) {
+			setErro(err?.response?.data?.error || 'Erro ao cadastrar usuário')
+		} finally {
+			setCarregando(false)
+		}
+	}
+
+
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-slate-900 p-4">
+			<h1 className="text-2xl font-bold mb-4">Criar uma conta</h1>
+			<form onSubmit={handleCadastro} noValidate className="w-3/12 flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md">
+				<div>
+					<label className="block text-sm text-slate-700 font-medium">Nome</label>
+					<input
+						type="text"
+						value={nome}
+						onChange={(e) => setNome(e.target.value)}
+						required
+						className="w-full p-2 border rounded text-slate-800 border-slate-300 focus:outline-none focus:ring-1 focus:ring-slate-400"
+					/>
+				</div>
+
+				<div>
+					<label className="block text-sm text-slate-700 font-medium">E-mail</label>
+					<input
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+						className={`w-full p-2 border rounded text-slate-800 border-slate-300 focus:outline-none focus:ring-1 focus:ring-slate-400 ${emailErro ? 'border-red-500' : ''}`}
+					/>
+					{emailErro && <p className="text-red-500 text-sm">{emailErro}</p>}
+				</div>
+
+				<div>
+					<label className="block text-sm text-slate-700 font-medium">Senha</label>
+					<div className="relative">
+						<input
+							type={mostrarSenha ? 'text' : 'password'}
+							value={senha}
+							onChange={(e) => setSenha(e.target.value)}
+							required
+							className="w-full p-2 border rounded text-slate-800 border-slate-300 focus:outline-none focus:ring-1 focus:ring-slate-400 pr-10"
+						/>
+						<button type="button" onClick={() => setMostrarSenha(!mostrarSenha)} className="bg-slate-200 rounded-full p-1 absolute right-1.5 top-1.5 text-sm text-slate-800 cursor-pointer">
+							{mostrarSenha ? <EyeOff size={20} /> : <Eye size={20} />}
+						</button>
+					</div>
+				</div>
+
+				{erro && <p className="text-red-500 text-sm">{erro}</p>}
+
+				<button type="submit" disabled={carregando} className="disabled:opacity-50 w-full font-semibold text-white p-2 rounded bg-orange-500 hover:bg-orange-600 cursor-pointer transition duration-200">
+					{carregando ? 'Aguarde...' : 'Cadastrar'}
+				</button>
+			</form>
+		</div>
+	)
+}
